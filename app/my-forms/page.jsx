b926a57af363637dadbbf92f46075c29bb68475a
GR-2004 <ganeshrana6377772039@gmail.com>
2025-03-29 19:49:34 +0530

    change button to createForm component

--- app/my-forms/page.jsx
+++ app/my-forms/page.jsx
@@ -1,10 +1,12 @@
+"use client";
 import React from "react";
 import CreateForm from "./_components/CreateForm";
 import FormList from "./_components/FormList";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function DashboardPage() {
+  const router = useRouter();
   return (
     <div className="p-8 flex flex-col gap-8">
       <div className="flex items-center gap-8">
@@ -15,10 +17,8 @@ export default function DashboardPage() {
           </p>
         </div>
         <div className="flex gap-4 items-center">
-          <Button>
-            Create Form <Plus />
-          </Button>
-          <Button variant="outline">View Templates</Button>
+          <CreateForm />
+          <Button variant="outline" onClick={() => router.push("/templates")}>View Templates</Button>
         </div>
       </div>
       <FormList />

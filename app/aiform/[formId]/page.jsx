44b6f5fd603ad60021fdbb3d16fb39a1a36acd76
Rituraj Jha <riturajreal01@gmail.com>
2025-03-31 10:30:14 +0530

    improved ui : form page

--- app/aiform/[formId]/page.jsx
+++ app/aiform/[formId]/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import React, { useEffect, useState } from "react";
 import { db } from "../../../config";
 import { JsonForms } from "../../../config/schema";
@@ -9,66 +9,79 @@ import FormUi from "../../my-forms/edit-form/_components/FormUi";
 import { Loader } from "lucide-react";
 
 const LiveAiForm = ({ params }) => {
-    const [record, setRecord] = useState(null);
-    const [jsonForm, setJsonForm] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [record, setRecord] = useState(null);
+  const [jsonForm, setJsonForm] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        params && GetFormData();
-    }, [params]);
+  useEffect(() => {
+    params && GetFormData();
+  }, [params]);
 
-    const GetFormData = async () => {
-        setLoading(true);
-        try {
-            const result = await db
-                .select()
-                .from(JsonForms)
-                .where(eq(JsonForms.id, Number(params?.formId)));
-            setRecord(result[0]);
-            setJsonForm(JSON.parse(result[0].jsonform));
-            setLoading(false);
-        } catch (error) {
-            setError(error);
-            setLoading(false);
-        }
-    };
+  const GetFormData = async () => {
+    setLoading(true);
+    try {
+      const result = await db
+        .select()
+        .from(JsonForms)
+        .where(eq(JsonForms.id, Number(params?.formId)));
+      setRecord(result[0]);
+      setJsonForm(JSON.parse(result[0].jsonform));
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
 
-    return (
-        <div
-            className="p-6 min-h-screen flex flex-col justify-center items-center"
-            style={{
-                background: record?.background,
-            }}
-        >
-            {loading ? (
-                <div className="flex justify-center items-center">
-                    <Loader className="h-6 w-6 animate-spin text-gray-600 dark:text-gray-300" />
-                </div>
-            ) : (
-                <>
-                    <Image src={"/logo.svg"} alt="logo" className="mb-6" width={260} height={26} />
-                    {record && (
-                        <FormUi
-                            jsonForm={jsonForm}
-                            selectedStyle={JSON.parse(record?.style)}
-                            selectedTheme={record?.theme}
-                            editable={false}
-                            formId={record?.id}
-                            enabledSignIn={record?.enabledSignIn}
-                        />
-                    )}
-                    <Link
-                        className="flex gap-2 items-center bg-black text-white px-3 py-1 rounded-full fixed bottom-5 left-5 cursor-pointer"
-                        href={'/'}
-                    >
-                        <Image src={"/badge-logo.webp"} alt="logo" width={50} height={50} className="rounded-full" />
-                        Build your own AI Form
-                    </Link>
-                </>
-            )}
+  return (
+    <div
+      className="p-4 md:p-6 max-w-3xl mx-auto min-h-screen"
+      style={{
+        background: record?.background,
+      }}
+    >
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <Loader className="h-6 w-6 animate-spin text-muted-foreground" />
         </div>
-    )
+      ) : (
+        <>
+          <div className="flex flex-col gap-6 items-center">
+            {record && (
+              <FormUi
+                jsonForm={jsonForm}
+                selectedStyle={JSON.parse(record?.style)}
+                selectedTheme={record?.theme}
+                editable={false}
+                formId={record?.id}
+                enabledSignIn={record?.enabledSignIn}
+              />
+            )}
+
+            <Link
+              href={"/"}
+              className="flex gap-2 items-center bg-muted/20 border p-2 rounded-badge "
+            >
+              <Image
+                src={"/logo-icon.svg"}
+                alt="logo"
+                width={64}
+                height={64}
+                className="rounded-full"
+              />
+              <div className="flex flex-col w-full">
+                <span className="text-lg font-bold">Formify</span>
+                <span className="text-sm text-muted-foreground">
+                  Build your AI Form
+                </span>
+              </div>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default LiveAiForm;
